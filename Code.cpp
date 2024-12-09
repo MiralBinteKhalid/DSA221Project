e9ff6994688cf3cde9165f31bad32873e9baf99e
ayesa-abid <u2023143@giki.edu.pk>
2024-12-10 01:33:40 +0500

    Update Code.cpp

--- Code.cpp
+++ Code.cpp
@@ -1,4 +1,18 @@
-#include<iostrem>
+#include<iostream>
+#include<fstream>
 using namepspace std;
+const int MAX_NODES=1000;
+const int MAX_EDGES=1000;
+//making edges in linkedlist 
+struct egde{
+int source ;
+int destination;
+int bandwidth;
+edge* next;
+egde(int sour,int dest,int btw)
 {
-
+source=sour;
+destination=dest;
+bandwidth=btw;
+}
+};
