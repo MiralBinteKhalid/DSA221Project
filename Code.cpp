7b4d01d968b8c4543d2ee25c2c2d33e0e0b3266c
MiralBinteKhalid <u2023321@giki.edu.pk>
2024-12-15 21:15:02 +0500

    Update Code.cpp
    update

--- Code.cpp
+++ Code.cpp
@@ -70,7 +70,7 @@ void merge(vector<SortedEdge>& edges, int left, int mid, int right) {
  vector<SortedEdge> L(n1), R(n2);
  for (int i = 0; i < n1; i++)
             L[i] = edges[left + i];
-        for (int i = 0; i < n2; i++)
+ for (int i = 0; i < n2; i++)
             R[i] = edges[mid + 1 + i];
 
 
